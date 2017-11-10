688d814ac887b7ca94fc151c35fbefd2ff76623e
mean pt <meanpt@gmail.com>
2017-11-10 21:16:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 let currentUser = "MeanPT"  ;
 let welcomeMessage = "Welcome to Flatbook, "  + currentUser  + "!";
 let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase()  + "!";
+let shortGreeting = `Welcome, ${welcomeMessage.charAt(0)}!` ;
